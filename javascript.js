a87a2d14df39984ee21bbc9f06abf1a1fd318ff5
sestion <6144926+sestion@users.noreply.github.com>
2025-11-22 18:46:59 +0800

    feat: add getHumanChoice

--- javascript.js
+++ javascript.js
@@ -1,7 +1,26 @@
 function getComputerChoice() {
-    let randomInt = Math.floor(Math.random() * 3);
+    let computerChoice = Math.floor(Math.random() * 3);
 
-    switch (randomInt) {
+    switch (computerChoice) {
+        case 0:
+            return "Rock";
+        case 1:
+            return "Paper";
+        case 2:
+            return "Scissors";
+        default:
+            return undefined;
+    }
+}
+
+function getHumanChoice() {
+    let playerChoice = parseInt(
+        prompt(
+            "Input a number between 0 and 2 \n0 = Rock, 1 = Paper, 2 = Scissors"
+        )
+    );
+
+    switch (playerChoice) {
         case 0:
             return "Rock";
         case 1:
