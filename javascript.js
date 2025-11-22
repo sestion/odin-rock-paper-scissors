19d01ad4f8334e287c9b47f95e21e0606567e66d
sestion <6144926+sestion@users.noreply.github.com>
2025-11-22 18:34:14 +0800

    feat: add getComputerChoice

--- javascript.js
+++ javascript.js
@@ -1,1 +1,14 @@
-console.log("Hello World");
+function getComputerChoice() {
+    let randomInt = Math.floor(Math.random() * 3);
+
+    switch (randomInt) {
+        case 0:
+            return "Rock";
+        case 1:
+            return "Paper";
+        case 2:
+            return "Scissors";
+        default:
+            return undefined;
+    }
+}
