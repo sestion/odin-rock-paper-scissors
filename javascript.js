b066b8c664786b11324dab5498700c5bf06f9b20
sestion <6144926+sestion@users.noreply.github.com>
2025-11-23 10:32:19 +0800

    feat: add logic to play a single round

--- javascript.js
+++ javascript.js
@@ -30,3 +30,42 @@ function getHumanChoice() {
 
 let humanScore = 0;
 let computerScore = 0;
+
+function getResult(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        return "Draw";
+    } else if (humanChoice === "Rock") {
+        return computerChoice === "Scissors" ? "Win" : "Lose";
+    } else if (humanChoice === "Paper") {
+        return computerChoice === "Rock" ? "Win" : "Lose";
+    } else if (humanChoice === "Scissors") {
+        return computerChoice === "Paper" ? "Win" : "Lose";
+    } else {
+        return "getResult() Invalid state!";
+    }
+}
+
+function getMessage(humanChoice, computerChoice) {
+    let result = getResult(humanChoice, computerChoice);
+
+    switch (result) {
+        case "Draw":
+            return `You draw! ${humanChoice} ties ${computerChoice}`;
+        case "Win":
+            return `You win! ${humanChoice} beats ${computerChoice}`;
+        case "Lose":
+            return `You lose! ${computerChoice} beats ${humanChoice}`;
+        default:
+            return "getMessage() Invalid state!";
+    }
+}
+
+function playRound(humanChoice, computerChoice) {
+    let message = getMessage(humanChoice, computerChoice);
+    console.log(message);
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
